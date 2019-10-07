9e22f6d8da8e8e447e19ffe1769b53ff12a10495
fmihel <fmihel@gmail.com>
2019-10-07 09:52:56 +0300

    add app

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
 import './template/define.css';
 import './template/main.css';
-import Common from 'Com/common.jsx';
-import { DOM } from 'fmihel-lib';
+import {
+    DOM, flex, flexChild, binds,
+} from 'fmihel-lib';
 import React from 'react';
 import ReacDOM from 'react-dom';
 
+class App extends React.Component {
+    constructor(p) {
+        super(p);
+    }
+
+    render() {
+        return (
+            <div id='app' style={{ ...flex(), ...flexChild() }}>
+
+            </div>
+        );
+    }
+}
+
 $(() => {
-    ReacDOM.render(<Common caption = "Test react"/>, DOM('#page'));
+    ReacDOM.render(<App/>, DOM('#page'));
 });
